46c74249f017893199ae12abb29a4eef80cab9df
kunalthadani <k.thadani14@gmail.com>
2020-01-06 16:42:39 +0530

    keeploopin

--- script.js
+++ script.js
@@ -2,21 +2,28 @@ document.addEventListener('DOMContentLoaded',function () {
 	console.log('Loaded');
 	txt = "#keeploopin";
 	loop_txt = document.querySelector("#typewrite");
-	i = 0;
+	
 	function dp(){
+		
 		if(i < txt.length){
 			loop_txt.innerHTML = loop_txt.innerHTML + txt.charAt(i);
 			console.log('Hello');
 			i++;
 			setTimeout(dp,200);
 		}
-		else if(i == txt.length){
+		/*else if(i == txt.length){
 			loop_txt.innerHTML = "";
 			i = 0;
 			setTimeout(dp,200);
-		}
+		}*/
 	}
+	i = 0;
 	dp();
+	setInterval(function(){
+		i = 0;
+		loop_txt.innerHTML = "";
+		dp();
+	},4000);
 	
 	console.log('yes');
 	document.querySelector('#dropper').onclick = function(){

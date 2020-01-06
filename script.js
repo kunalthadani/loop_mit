70d4a8f457e1e05d34f6db7d60a7c9e544c18af1
kunalthadani <k.thadani14@gmail.com>
2020-01-06 15:53:57 +0530

    pages fix

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded',function () {
+	console.log('Loaded');
+	txt = "#keeploopin";
+	loop_txt = document.querySelector("#typewrite");
+	i = 0;
+	function dp(){
+		if(i < txt.length){
+			loop_txt.innerHTML = loop_txt.innerHTML + txt.charAt(i);
+			console.log('Hello');
+			i++;
+			setTimeout(dp,200);
+		}
+	}
+	dp();
+	console.log('yes');
+	document.querySelector('#dropper').onclick = function(){
+		console.log("OK");
+		if(document.querySelector('#dropdown').style.display == 'block')
+			document.querySelector('#dropdown').style.display = 'none';
+		else{
+			console.log("Hello");
+			document.querySelector('#dropdown').style.display = 'block';
+		}
+	}
+});
